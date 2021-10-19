46ba007e35bcb96fe2de814c33b0829eeb629b9d
Tellfisk <tellev@outlook.com>
2021-10-19 16:22:39 +0200

    Added musicplayer widget and Tour mp3

--- lib/main.dart
+++ lib/main.dart
@@ -11,8 +11,8 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: const Text('Welcome to Flutter'),
         ),
-        body: const Center(
-          child: Text('Hello World'),
+        body: const Column (
+          children: Text('Hello World'),
         ),
       ),
     );
